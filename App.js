c3de136c26c81167b128ed4690142914ffd920b5
Stanislav Popadynets <popadynetss@gmail.com>
2021-02-03 13:29:05 +0200

    Add basic UI layout and primitive operations functionality

--- App.js
+++ App.js
@@ -1,11 +1,82 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
-export default function App() {
+import Digit from './src/Digit';
+import Operation from './src/Operation';
+import Orange from './src/Orange';
+
+const operations = ['/', 'm+', 'x', '+', '-', '='];
+const digits = [',', ...Array(10).keys()];
+
+ const App = () => {
+
+  const [firstVal, setFirstVal] = useState('');
+  const [secondVal, setSecondtVal] = useState('');
+  const [operation, setOperation] = useState('');
+  const [result, setResult] = useState(null);
+
+  const onDigitClick = digit => {
+    debugger
+    if ((!firstVal || (firstVal && !operation && !secondVal))) {
+      setFirstVal(Number(firstVal + String(digit)));
+      setResult(Number(firstVal + String(digit)));
+    } else {
+      setSecondtVal(Number(secondVal + String(digit)));
+      setResult(Number(secondVal + String(digit)));
+    }
+  }
+
+  const onOperation = () => {
+    let res;
+    switch (operation) {
+      case '/' : res = firstVal / secondVal;
+        break;
+      case '+' : res = firstVal + secondVal;
+        break;
+      case 'x' : res = firstVal * secondVal;
+        break;
+      case '-' : res = firstVal - secondVal;
+        break;
+      default : console.log(res);
+      }
+      console.log('RESULT', res)
+      setFirstVal(res);
+      setResult(res);
+      setSecondtVal('');
+      setOperation('');
+    } 
+  // console.log(firstVal)
+  // console.log(secondVal)
+  // console.log("APPPPPPPPPPPPPPPPp")
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput textAlign='center' style={{textAlign: 'right'}} value={result ?? '0'}/>
+      <View style={{flexDirection: 'row'}}>
+        
+        <View style={styles.digitContainer}>
+          {/* <Digit value=',' style={{flexOrder: 1}} /> */}
+          {digits.map(el => <Digit onPress={() => onDigitClick(el)} key={el} value={el}/> )}
+          <View style={{flexDirection: 'row', width: 120, flexWrap: 'wrap'}}>
+            <Operation value='AC' />
+            <Operation value='+/-' />
+            <Operation value='%' />
+            <Operation value='mc' />
+            <Operation value='mr' />
+            <Operation value='m-' />
+          </View>
+        </View>
+        <View>
+          {operations.map(el => <Orange key={el} value={el} onPress={() => {
+            if(el === '=') {
+              onOperation();
+            }
+            else {
+              setOperation(el);
+            }
+          }} />)}
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +89,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  digitContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap-reverse',
+    width: 120
+  }
 });
+
+export default App;
---
+++ src/Operation.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+const Opeation = (props) => {
+
+  return (
+    <TouchableOpacity style={styles.button}>
+      <Text style={styles.title}>{props.value}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 50,
+    backgroundColor: '#333333',
+    padding: 10,
+    width: 40,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    color: '#fff'
+  }
+});
+
+export default Opeation;
+
+// #ff9812 orange
+// #333333 digit
+// #a5a5a5 gray
